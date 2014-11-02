cab19353736be13d4813c42e6295e83b36301b2a
Ishbir Singh <ishbir@ishbir.com>
2014-11-01 20:37:02 -0400

    First commit

---
+++ bitmessage/identity.go
@@ -0,0 +1,16 @@
+/*
+Responsible for creation and management of user identities.
+*/
+package identity
+
+import (
+	"crypto/sha512"
+)
+
+struct Identity {
+	
+}
+
+func NewRandom(short) *Identity {
+	
+}

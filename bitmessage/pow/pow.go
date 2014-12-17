3ac2c4580266e52740b6a87a204a5481beca3c06
Ishbir Singh <ishbir@ishbir.com>
2014-12-17 22:19:07 +0530

    Basic work on POW

--- bitmessage/pow/pow.go
+++ bitmessage/pow/pow.go
@@ -1,15 +1,62 @@
+// pow package is responsible for proof of work calculation/verification for an
+// object transmitted over the network.
 package pow
 
-// Calculate the target POW value.
-// https://bitmessage.org/wiki/Proof_of_work
-func calcTarget() float64 {
+import (
+	"bytes"
+	"crypto/sha512"
+	"encoding/binary"
+	"time"
 
+	"github.com/ishbir/bmgo/bitmessage/protocol"
+)
+
+// CalculateTarget calculates the target POW value.
+// https://bitmessage.org/wiki/Proof_of_work
+func CalculateTarget(PayloadLength, PayloadLengthExtraBytes, TTL,
+	NonceTrialsPerByte int) int64 {
+	payloadLength := float64(PayloadLength)
+	payloadLengthExtraBytes := float64(PayloadLengthExtraBytes)
+	ttl := float64(TTL)
+	nonceTrialsPerByte := float64(NonceTrialsPerByte)
+
+	return int64(float64(2^64) / (nonceTrialsPerByte * (payloadLength +
+		payloadLengthExtraBytes + ((ttl * (payloadLength + payloadLengthExtraBytes)) /
+		float64(2^16)))))
 }
 
 // Check if the POW is sufficient for an object.
-func IsSufficient(object []byte,
-	ttl, nonceTrialsPerByte, payloadLengthExtraBytes uint) bool {
+func CheckPOW(objMessage *protocol.ObjectMessage, PayloadLengthExtraBytes,
+	NonceTrialsPerByte int) bool {
+	ttl := int(objMessage.ExpiresTime - uint64(time.Now().Unix()))
+	data := objMessage.Serialize()
+	dataToCheck := data[8:] // exclude the nonce value in the beginning
+	payloadLength := len(dataToCheck)
 
+	hash := sha512.New()
+	hash.Write(dataToCheck)
+	initialHash := hash.Sum(nil)
+
+	hash.Reset()
+	hash.Write(data[0:8]) // nonce
+	hash.Write(initialHash)
+	tempHash := hash.Sum(nil)
+	hash.Reset()
+	hash.Write(tempHash)
+	resultHash := hash.Sum(nil)
+
+	b := bytes.NewReader(resultHash[0:8])
+	var powValue int64
+	binary.Read(b, binary.BigEndian, &powValue)
+
+	target := CalculateTarget(payloadLength, PayloadLengthExtraBytes, ttl,
+		NonceTrialsPerByte)
+
+	if powValue <= target {
+		return true
+	} else {
+		return false
+	}
 }
 
 // Do the POW using multiple go-routines

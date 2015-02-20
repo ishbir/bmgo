fd67f6275acb204c830b2187b883fe504f785f19
Ishbir Singh <ishbir@ishbir.com>
2015-02-19 23:54:28 -0500

    Moved object message handling code and tests to different files.

---
+++ bitmessage/protocol/object_message.go
@@ -0,0 +1,244 @@
+package protocol
+
+import (
+	"bytes"
+	"crypto/sha512"
+	"encoding/binary"
+	"errors"
+	"io"
+	"math/rand"
+	"time"
+
+	"github.com/ishbir/elliptic"
+
+	"github.com/ishbir/bmgo/bitmessage/constants"
+	"github.com/ishbir/bmgo/bitmessage/identity"
+	"github.com/ishbir/bmgo/bitmessage/pow"
+	"github.com/ishbir/bmgo/bitmessage/protocol/objects"
+	"github.com/ishbir/bmgo/bitmessage/protocol/types"
+)
+
+// HeaderSerialize is responsible for serializing the object message header
+// (excluding the nonce) for use in signing of the payload.
+func (msg *ObjectMessage) HeaderSerialize() []byte {
+	var b bytes.Buffer
+	binary.Write(&b, binary.BigEndian, msg.expiresTime)
+	binary.Write(&b, binary.BigEndian, uint32(msg.ObjectType))
+	b.Write(msg.Version.Serialize())
+	b.Write(msg.Stream.Serialize())
+	return b.Bytes()
+}
+
+func (msg *ObjectMessage) Serialize() []byte {
+	var b bytes.Buffer
+
+	binary.Write(&b, binary.BigEndian, msg.Nonce)
+	b.Write(msg.HeaderSerialize())
+	b.Write(msg.Payload.Serialize())
+
+	return CreateMessage("object", b.Bytes())
+}
+
+func (msg *ObjectMessage) DeserializeReader(b io.Reader) error {
+	err := binary.Read(b, binary.BigEndian, &msg.Nonce)
+	if err != nil {
+		return types.DeserializeFailedError("nonce: " + err.Error())
+	}
+	err = binary.Read(b, binary.BigEndian, &msg.expiresTime)
+	if err != nil {
+		return types.DeserializeFailedError("expiresTime: " + err.Error())
+	}
+	var objType uint32
+	err = binary.Read(b, binary.BigEndian, &objType)
+	if err != nil {
+		return types.DeserializeFailedError("objectType: " + err.Error())
+	}
+	msg.ObjectType = ObjectType(objType)
+	err = msg.Version.DeserializeReader(b)
+	if err != nil {
+		return types.DeserializeFailedError("version: " + err.Error())
+	}
+	err = msg.Stream.DeserializeReader(b)
+	if err != nil {
+		return types.DeserializeFailedError("stream: " + err.Error())
+	}
+	msg.setPayloadType() // set the Payload field
+	err = msg.Payload.DeserializeReader(b)
+	if err != nil {
+		return types.DeserializeFailedError("payload" + err.Error())
+	}
+	return nil
+}
+
+// random generates a random int between the given range
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+// Preserialize is responsible for embedding the public signing and encryption
+// keys, setting the POW parameters like nonce trials per byte and extra bytes,
+// signing the unencrypted message, encrypting the object, setting the tags,
+// calculating and setting the expiration time and then doing POW. The function
+// is meant to be used only when the object is created from scratch, not when
+// it is being propogated.
+func (msg *ObjectMessage) Preserialize(id *identity.Own,
+	target *identity.Foreign) error {
+	// calculate TTL of the object message based on the defined constant values
+	msg.TTL += time.Second * time.Duration(random(
+		-int(time.Duration(constants.ObjectTTLRandRange).Seconds()),
+		int(time.Duration(constants.ObjectTTLRandRange).Seconds())))
+	ttl := int(msg.TTL.Seconds())
+	// set the expiration time based on TTL
+	msg.expiresTime = uint64(time.Now().Add(msg.TTL).Unix())
+
+	// Setting signing and encryption public keys
+	if keysetter, ok := msg.Payload.(PublicKeysAddablePayload); ok {
+		keysetter.SetSigningAndEncryptionKeys(
+			id.SigningKey.PublicKey.SerializeUncompressed()[1:], // exclude 0x04
+			id.EncryptionKey.PublicKey.SerializeUncompressed()[1:],
+		)
+	}
+
+	// Message signing
+	if signer, ok := msg.Payload.(SignablePayload); ok {
+		var b bytes.Buffer
+		b.Write(msg.HeaderSerialize())
+		b.Write(signer.SignatureSerialize())
+		signature, err := id.SigningKey.Sign(b.Bytes())
+		if err != nil {
+			return errors.New("signing failed: " + err.Error())
+		}
+		signer.SetSignature(signature)
+	}
+
+	// tag, if it needs to be added
+	var tag []byte
+
+	switch msg.Payload.(type) {
+	// set the encryption key and tag for v4 pubkeys and v5 broadcasts
+	case *objects.PubkeyUnencryptedV4, *objects.BroadcastUnencryptedV5:
+		hash := id.Address.CalcDoubleHash()
+		privKey, err := elliptic.PrivateKeyFromRawBytes(elliptic.Secp256k1,
+			hash[:32])
+		if err != nil {
+			return errors.New("failed to create private key from address: " +
+				err.Error())
+		}
+		target = new(identity.Foreign)
+		target.EncryptionKey = &privKey.PublicKey
+		tag = hash[32:] // set the tag
+
+	// set encryption key for v4 broadcasts
+	case *objects.BroadcastUnencryptedV4AddressV2,
+		*objects.BroadcastUnencryptedV4AddressV3:
+		sha := sha512.New()
+		sha.Write(id.Address.Version.Serialize())
+		sha.Write(id.Address.Stream.Serialize())
+		sha.Write(id.Address.Ripe[:])
+		hash := sha.Sum(nil)
+
+		privKey, err := elliptic.PrivateKeyFromRawBytes(elliptic.Secp256k1,
+			hash[:32])
+		if err != nil {
+			return errors.New("failed to create private key from address: " +
+				err.Error())
+		}
+		target = new(identity.Foreign)
+		target.EncryptionKey = &privKey.PublicKey
+
+	// set network default POW parameters for objects that are meant to not be
+	// destination client specific
+	case *objects.PubkeyV2, *objects.PubkeyV3,
+		*objects.GetpubkeyV3, *objects.GetpubkeyV4:
+		target = new(identity.Foreign)
+
+	}
+	// Enforce minimum POW requirements. This also sets POW parameters for
+	// foreign identities generated previously.
+	if target.NonceTrialsPerByte < constants.POWDefaultNonceTrialsPerByte {
+		target.NonceTrialsPerByte = constants.POWDefaultNonceTrialsPerByte
+	}
+	if target.ExtraBytes < constants.POWDefaultNonceTrialsPerByte {
+		target.ExtraBytes = constants.POWDefaultNonceTrialsPerByte
+	}
+
+	// encrypt the payload if it needs to be encrypted
+	if unenc, ok := msg.Payload.(EncryptablePayload); ok {
+		enc, err := unenc.Encrypt(target.EncryptionKey)
+		if err != nil {
+			return errors.New("payload encryption failed: " + err.Error())
+		}
+		msg.Payload = enc // set payload to encrypted object
+	}
+
+	// add a tag to the payload if it needs to be added
+	if taggable, ok := msg.Payload.(TaggableEncryptedPayload); ok {
+		taggable.SetTag(tag)
+	}
+	// Our payload is ready. Do POW.
+	payload := msg.Payload.Serialize()
+	objHeader := msg.HeaderSerialize()
+	payloadLength := len(payload) + len(objHeader) + 8 // nonce length also
+
+	powTarget := pow.CalculateTarget(payloadLength, ttl,
+		int(target.NonceTrialsPerByte), int(target.ExtraBytes))
+
+	hash := sha512.New()
+	hash.Write(msg.HeaderSerialize())
+	hash.Write(payload)
+	// TODO add logic for choosing which POW implementation to use
+	msg.Nonce = pow.DoSequential(powTarget, hash.Sum(nil))
+	return nil
+}
+
+// setPayloadType sets the Payload field of the ObjectMessage struct according
+// to the value of Version and ObjectType.
+func (msg *ObjectMessage) setPayloadType() {
+	// default value
+	msg.Payload = &objects.Unrecognized{}
+	// used when the msg version is unknown
+	corrupt := &objects.Corrupt{}
+
+	switch msg.ObjectType {
+	case GetpubkeyObject:
+		switch msg.Version {
+		case 2:
+			fallthrough
+		case 3:
+			msg.Payload = &objects.GetpubkeyV3{}
+		case 4:
+			msg.Payload = &objects.GetpubkeyV4{}
+		default:
+			msg.Payload = corrupt
+		}
+
+	case PubkeyObject:
+		switch msg.Version {
+		case 2:
+			msg.Payload = &objects.PubkeyV2{}
+		case 3:
+			msg.Payload = &objects.PubkeyV3{}
+		case 4:
+			msg.Payload = &objects.PubkeyEncryptedV4{}
+		default:
+			msg.Payload = corrupt
+		}
+
+	case MsgObject:
+		if int(msg.Version) == 1 { // this has been fixed at 1
+			msg.Payload = &objects.MsgEncrypted{}
+		} else {
+			msg.Payload = corrupt
+		}
+
+	case BroadcastObject:
+		switch msg.Version {
+		case 4:
+			msg.Payload = &objects.BroadcastEncryptedV4{}
+		case 5:
+			msg.Payload = &objects.BroadcastEncryptedV5{}
+		default:
+			msg.Payload = corrupt
+		}
+	}
+}
---
+++ bitmessage/protocol/object_message_test.go
@@ -0,0 +1,357 @@
+package protocol
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/ishbir/elliptic"
+
+	"github.com/ishbir/bmgo/bitmessage/constants"
+	"github.com/ishbir/bmgo/bitmessage/identity"
+	"github.com/ishbir/bmgo/bitmessage/pow"
+	"github.com/ishbir/bmgo/bitmessage/protocol/objects"
+	"github.com/ishbir/bmgo/bitmessage/protocol/types"
+)
+
+var ownId1 *identity.Own
+var ownId2 *identity.Own
+
+func init() {
+	var err error
+	ownId1, err = identity.NewRandom(1)
+	if err != nil {
+		panic("failed to generate identity 1")
+	}
+	ownId2, err = identity.NewRandom(1)
+	if err != nil {
+		panic("failed to generate identity 2")
+	}
+	ownId1.CreateAddress(4, 1)
+	ownId2.CreateAddress(4, 1)
+}
+
+func TestObjectMessage(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: ObjectType(6), // undefined
+		Version:    types.Varint(8),
+		Stream:     types.Varint(1),
+		Payload:    &bytesPayload{bytes: []byte{0x54, 0xA4, 0x4E, 0x9F}},
+	}
+	err := msg.Preserialize(nil, ownId1.ToForeign())
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	raw := msg.Serialize()
+
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.Nonce != msg.Nonce {
+		t.Error("for Nonce got", msg1.Nonce, "expected", msg.Nonce)
+	}
+	if msg1.expiresTime != msg.expiresTime {
+		t.Error("for expiresTime got", msg1.expiresTime, "expected",
+			msg.expiresTime)
+	}
+	if msg1.ObjectType != msg.ObjectType {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected",
+			msg.ObjectType)
+	}
+	if msg1.Version != msg.Version {
+		t.Error("for Version got", msg1.Version, "expected", msg.Version)
+	}
+	if msg1.Stream != msg.Stream {
+		t.Error("for Stream got", msg1.Stream, "expected", msg.Stream)
+	}
+	if _, ok := msg1.Payload.(*objects.Unrecognized); !ok {
+		t.Error("for Payload, did not get Unrecognized object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], int(ownId1.ExtraBytes),
+		int(ownId1.NonceTrialsPerByte)) {
+		t.Error("nonce check failed")
+	}
+}
+
+func TestGetpubkeyV4Object(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: GetpubkeyObject,
+		Version:    4,
+		Stream:     1,
+		Payload: &objects.GetpubkeyV4{
+			Tag: ownId2.Address.Tag(),
+		},
+	}
+	err := msg.Preserialize(nil, nil)
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	raw := msg.Serialize()
+
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.ObjectType != GetpubkeyObject {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected GetpubkeyObject")
+	}
+	if _, ok := msg1.Payload.(*objects.GetpubkeyV4); !ok {
+		t.Error("for Payload, did not get GetpubkeyV4 object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], constants.POWDefaultExtraBytes,
+		constants.POWDefaultNonceTrialsPerByte) {
+		t.Error("nonce check failed")
+	}
+	if tag := ownId2.Address.Tag(); !bytes.Equal(tag[:],
+		msg1.Payload.(*objects.GetpubkeyV4).Tag[:]) {
+		t.Error("tags didn't match")
+	}
+}
+
+func TestGetpubkeyV3Object(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: GetpubkeyObject,
+		Version:    3,
+		Stream:     1,
+		Payload: &objects.GetpubkeyV3{
+			Ripe: ownId2.Address.Ripe,
+		},
+	}
+	err := msg.Preserialize(nil, nil)
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	raw := msg.Serialize()
+
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.ObjectType != GetpubkeyObject {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected GetpubkeyObject")
+	}
+	if _, ok := msg1.Payload.(*objects.GetpubkeyV3); !ok {
+		t.Error("for Payload, did not get GetpubkeyV3 object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], constants.POWDefaultExtraBytes,
+		constants.POWDefaultNonceTrialsPerByte) {
+		t.Error("nonce check failed")
+	}
+	if !bytes.Equal(ownId2.Address.Ripe[:],
+		msg1.Payload.(*objects.GetpubkeyV3).Ripe[:]) {
+		t.Error("ripe didn't match")
+	}
+}
+
+func TestPubkeyV2Object(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: PubkeyObject,
+		Version:    2,
+		Stream:     1,
+		Payload: &objects.PubkeyV2{
+			Behaviour: (0x01 << 31), // we need ack
+		},
+	}
+	ownId1.CreateAddress(2, 1)
+	err := msg.Preserialize(ownId1, nil) // set our keys
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	raw := msg.Serialize()
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.ObjectType != PubkeyObject {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected PubkeyObject")
+	}
+	if _, ok := msg1.Payload.(*objects.PubkeyV2); !ok {
+		t.Error("for Payload, did not get GetpubkeyV2 object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], constants.POWDefaultExtraBytes,
+		constants.POWDefaultNonceTrialsPerByte) {
+		t.Error("nonce check failed")
+	}
+
+	payload := msg1.Payload.(*objects.PubkeyV2)
+
+	// generate foreign identity from public key and check if it's same
+	genID := new(identity.Foreign)
+	// errors here can be safely ignored (refer to elliptic.go for reason)
+	genID.EncryptionKey, _ = elliptic.PublicKeyFromUncompressedBytes(
+		elliptic.Secp256k1, append([]byte{0x04}, payload.PubEncryptionKey[:]...))
+	genID.SigningKey, _ = elliptic.PublicKeyFromUncompressedBytes(
+		elliptic.Secp256k1, append([]byte{0x04}, payload.PubSigningKey[:]...))
+	genID.CreateAddress(2, 1)
+	genID.SetDefaultPOWParams()
+
+	ownAddr, _ := ownId1.Address.Encode()
+	genAddr, _ := genID.Address.Encode()
+
+	if ownAddr != genAddr {
+		t.Error("for generated addresses expected", ownAddr, "got", genAddr)
+	}
+}
+
+func TestPubkeyV3Object(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: PubkeyObject,
+		Version:    3,
+		Stream:     1,
+		Payload: &objects.PubkeyV3{
+			Behaviour: (0x01 << 31), // we need ack
+		},
+	}
+	ownId1.CreateAddress(3, 1)
+	err := msg.Preserialize(ownId1, nil) // set our keys
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	p1 := msg.Payload.(*objects.PubkeyV3)
+
+	// check if public encryption key was set
+	if bytes.Equal(p1.PubEncryptionKey[:],
+		bytes.Repeat([]byte{0x00}, 64)) {
+		t.Error("public encryption key is empty")
+	}
+	// check if signing key was set
+	if bytes.Equal(p1.PubSigningKey[:],
+		bytes.Repeat([]byte{0x00}, 64)) {
+		t.Error("public signing key is empty")
+	}
+
+	raw := msg.Serialize()
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.ObjectType != PubkeyObject {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected PubkeyObject")
+	}
+	if _, ok := msg1.Payload.(*objects.PubkeyV3); !ok {
+		t.Error("for Payload, did not get GetpubkeyV3 object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], constants.POWDefaultExtraBytes,
+		constants.POWDefaultNonceTrialsPerByte) {
+		t.Error("nonce check failed")
+	}
+
+	payload := msg1.Payload.(*objects.PubkeyV3)
+
+	// generate foreign identity from public key and check if it's same
+	genID := new(identity.Foreign)
+	// errors here can be safely ignored (refer to elliptic.go for reason)
+	genID.EncryptionKey, _ = elliptic.PublicKeyFromUncompressedBytes(
+		elliptic.Secp256k1, append([]byte{0x04}, payload.PubEncryptionKey[:]...))
+	genID.SigningKey, _ = elliptic.PublicKeyFromUncompressedBytes(
+		elliptic.Secp256k1, append([]byte{0x04}, payload.PubSigningKey[:]...))
+	genID.CreateAddress(3, 1)
+	// no need to check these because they're going to be same if two payloads
+	// are byte equal
+	genID.NonceTrialsPerByte = payload.NonceTrialsPerByte
+	genID.ExtraBytes = payload.ExtraBytes
+
+	ownAddr, _ := ownId1.Address.Encode()
+	genAddr, _ := genID.Address.Encode()
+
+	if ownAddr != genAddr {
+		t.Error("for generated addresses expected", ownAddr, "got", genAddr)
+	}
+
+	// check if the signature is valid
+	sigMatch, err := genID.SigningKey.VerifySignature(payload.Signature,
+		append(msg1.HeaderSerialize(), payload.SignatureSerialize()...))
+	if err != nil {
+		t.Error("signature verification failed:", err)
+	}
+	if !sigMatch {
+		t.Error("invalid signature")
+	}
+
+}
+
+func TestPubkeyV4Object(t *testing.T) {
+	msg := ObjectMessage{
+		TTL:        time.Hour,
+		ObjectType: PubkeyObject,
+		Version:    4,
+		Stream:     1,
+		Payload: &objects.PubkeyUnencryptedV4{
+			objects.PubkeyV3{
+				Behaviour: (0x01 << 31), // we need ack
+			},
+		},
+	}
+
+	if _, ok := msg.Payload.(EncryptablePayload); !ok {
+		t.Fatal("cannot be encrypted")
+	}
+
+	err := msg.Preserialize(ownId1, nil) // set our keys
+	if err != nil {
+		t.Fatal("preserialize error:", err.Error())
+	}
+
+	// check if Preserialize encrypted successfully
+	if _, ok := msg.Payload.(*objects.PubkeyEncryptedV4); !ok {
+		t.Error("for Payload, did not get PubkeyEncryptedV4 object type")
+	}
+
+	raw := msg.Serialize()
+	msg1 := new(ObjectMessage)
+	DeserializeTo(msg1, raw[MessageHeaderSize():])
+
+	if msg1.ObjectType != PubkeyObject {
+		t.Error("for ObjectType got", msg1.ObjectType, "expected PubkeyObject")
+	}
+	if _, ok := msg1.Payload.(*objects.PubkeyEncryptedV4); !ok {
+		t.Error("for Payload, did not get PubkeyEncryptedV4 object type")
+	}
+	if !bytes.Equal(msg1.Payload.Serialize(), msg.Payload.Serialize()) {
+		t.Error("for Payload got", msg1.Payload.Serialize(), "expected",
+			msg.Payload.Serialize())
+	}
+	if !pow.Check(raw[MessageHeaderSize():], constants.POWDefaultExtraBytes,
+		constants.POWDefaultNonceTrialsPerByte) {
+		t.Error("nonce check failed")
+	}
+	// TODO try decrypting Pubkey and check if it corresponds to unencrypted
+	// key
+	// TODO check if the signature of unencrypted message is valid
+	// TODO generate foreign identity from public key and check if it's same
+
+}
+
+func TestMsgObject(t *testing.T) {
+
+}
+
+func TestBroadcastObject(t *testing.T) {
+
+}
